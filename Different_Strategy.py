6f717f08a0fd79cd887f1f1cc6b25f36b545b308
Newton_Fractal <51488709+NewtonFractal@users.noreply.github.com>
2019-07-14 08:23:33 +0200

    Update Different_Strategy.py

--- Different_Strategy.py
+++ Different_Strategy.py
@@ -2,7 +2,6 @@ import math
 import time
 
 primelist = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101]
-odd_composites = [9, 15, 21, 25, 27, 33, 35, 39, 45, 49, 51, 55, 57, 63, 65, 69, 75, 77, 81, 85, 87, 91, 93, 95, 99]
 start = time.time()
 
 def Goldbachs_other_conjecture(number):
